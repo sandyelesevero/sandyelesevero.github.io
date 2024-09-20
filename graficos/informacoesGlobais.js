9adbf4fbfed0948edd580717b4c4037e8c88f80a
sandyelesevero <108473385+sandyelesevero@users.noreply.github.com>
2024-09-20 12:06:46 -0300

    a

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -12,9 +12,6 @@ async function vizualizarInformacoesGlobais() {
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
-    console.log(paragrafo)  
 }
-
 
 vizualizarInformacoesGlobais()
